6f8bdfb498e2dd44283a1cb65f39211f5ef0416d
kumochan <thaotrinhminh@gmail.com>
2022-03-20 22:14:08 +0700

    vue meta tag

--- sourcecode/src/App.vue
+++ sourcecode/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id='app'>
+  <div id="app">
     <router-view />
   </div>
 </template>
@@ -12,6 +12,7 @@
 <script>
 export default {
   name: 'app',
+
   mounted() {
     const fontawesome = document.createElement('script');
     fontawesome.setAttribute(
@@ -20,42 +21,51 @@ export default {
     );
     document.head.appendChild(fontawesome);
   },
-  created() {
-    const scripts = [
-     '/assets/js/bootstrap.bundle.min.js',
-     '/assets/js/scripts.js',
-     '/assets/js/Chart.min.js',
-    //  '/assets/demo/chart-area-demo.js',
-    //  '/assets/demo/chart-bar-demo.js',
-     '/assets/js/simple-datatables@latest',
-     '/assets/js/datatables-simple-demo.js',
-    ];
-    scripts.forEach((script) => {
-      const tag = document.createElement('script');
-      tag.setAttribute('src', script);
-      document.head.appendChild(tag);
-    });
-  },
-  // metaInfo: {
-  //   title: 'Default App Title',
-  //   titleTemplate: '%s | vue-meta Example App',
-  //   htmlAttrs: {
-  //     lang: 'en-US',
-  //   },
-  //   meta: [
-  //     { charset: 'utf-8' },
-  //     { name: 'description', content: 'An example Vue application with vue-meta.' },
-  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //   ],
-  //   script: [
-  //     { src: '/assets/js/bootstrap.bundle.min.js' },
-  //     { src: '/assets/js/scripts.js' },
-  //     { src: '/assets/js/Chart.min.js' },
-  //     { src: '/assets/demo/chart-area-demo.js' },
-  //     { src: '/assets/demo/chart-bar-demo.js' },
-  //     { src: '/assets/js/simple-datatables@latest' },
-  //     { src: '/assets/js/datatables-simple-demo.js' },
-  //   ],
+  // created() {
+  //   const scripts = [
+  //     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js',
+  //     '/assets/js/bootstrap.bundle.min.js',
+  //     '/assets/js/scripts.js',
+  //     '/assets/js/Chart.min.js',
+  //     //  '/assets/demo/chart-area-demo.js',
+  //     //  '/assets/demo/chart-bar-demo.js',
+  //     '/assets/js/simple-datatables@latest',
+  //     '/assets/js/datatables-simple-demo.js',
+  //   ];
+  //   scripts.forEach((script) => {
+  //     const tag = document.createElement('script');
+  //     tag.setAttribute('src', script);
+  //     document.head.appendChild(tag);
+  //   });
   // },
+  metaInfo: {
+    title: 'Default App Title',
+    titleTemplate: '%s | vue-meta Example App',
+    htmlAttrs: {
+      lang: 'en-US',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'description',
+        content: 'An example Vue application with vue-meta.',
+      },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    script: [
+      {
+        src: '/assets/js/bootstrap.bundle.min.js',
+        type: 'text/javascript',
+        async: true,
+        defer: true,
+      },
+      { src: '/assets/js/scripts.js' },
+      { src: '/assets/js/Chart.min.js' },
+      { src: '/assets/demo/chart-area-demo.js' },
+      { src: '/assets/demo/chart-bar-demo.js' },
+      { src: '/assets/js/simple-datatables@latest' },
+      { src: '/assets/js/datatables-simple-demo.js' },
+    ],
+  },
 };
 </script>
--- sourcecode/src/store/index.js
+++ sourcecode/src/store/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueHead from 'vue-head';
 
 Vue.use(Vuex);
+Vue.use(VueHead);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+
+});
